31023db9333a26fe0c9c3c62f6070b7cf910dbe8
Blade <bladeprojectz@gmail.com>
2024-07-02 17:04:47 +0800

    DaisyUI

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -3,7 +3,7 @@ import AddToCart from "./AddToCart";
 
 const ProductCard = () => {
   return (
-    <div className="p-4 my-5 bg-sky-500 text-white text-2xl hover:bg-sky-700">
+    <div>
       <AddToCart />
     </div>
   );
